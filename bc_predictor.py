5cdcc051e65bfde148da79bd6dccefec1a3900ce
Alan Fu <alan.fu@mail.mcgill.ca>
2025-08-08 12:28:46 -0400

    bugfix

--- bc_predictor.py
+++ bc_predictor.py
@@ -7,7 +7,7 @@ from typing import Optional, Tuple, Union
 import torch
 import torch.utils.checkpoint
 from torch import nn
-# from torch.nn import BCEWithLogitsLoss, CrossEntropyLoss, MSELoss, SiLU
+from torch.nn import BCEWithLogitsLoss, CrossEntropyLoss, MSELoss, SiLU
 from torch.nn import CrossEntropyLoss, SiLU
 import torch.nn.functional as nnfunc
 # from transformers.file_utils import (
@@ -17,7 +17,7 @@ import torch.nn.functional as nnfunc
 from transformers.modeling_outputs import (
     BaseModelOutputWithPastAndCrossAttentions,
     BaseModelOutputWithPoolingAndCrossAttentions,
-    MaskedLMOutput,
+    MaskedLMOutput, SequenceClassifierOutput,
     # SequenceClassifierOutput,
     # TokenClassifierOutput,
 )
@@ -920,5 +920,56 @@ class MaskedLM(BasePreTrainedModel):
             attentions=outputs.attentions,
         )
 
-    # def predict_contacts(self, tokens, attention_mask):
-    #     return self.model.predict_contacts(tokens, attention_mask=attention_mask)
+class CancerClassificationModel(BasePreTrainedModel):
+    def __init__(self, config):
+        super().__init__(config)
+        self.num_labels = config.num_labels
+        self.model = HierarchicalGenomeTransformer(config)
+        self.classifier = nn.Sequential(
+            nn.Dropout(config.hidden_dropout_prob),
+            nn.Linear(config.hidden_size, config.num_labels)
+        )
+        self.loss_fct = BCEWithLogitsLoss()
+
+    def forward(
+            self,
+            input_ids: Optional[torch.LongTensor] = None,
+            attention_mask: Optional[torch.Tensor] = None,
+            inputs_embeds: Optional[torch.FloatTensor] = None,
+            encoder_hidden_states: Optional[torch.FloatTensor] = None,
+            encoder_attention_mask: Optional[torch.Tensor] = None,
+            labels: Optional[torch.LongTensor] = None,
+            output_hidden_states: Optional[bool] = None,
+            return_dict: Optional[bool] = None,
+            methylation_ids: Optional[torch.Tensor] = None,
+            age_ids: Optional[torch.Tensor] = None,
+    ):
+        return_dict = (
+            return_dict if return_dict is not None else self.config.use_return_dict
+        )
+
+        outputs = self.model(
+            input_ids,
+            attention_mask=attention_mask,
+            inputs_embeds=inputs_embeds,
+            encoder_hidden_states=encoder_hidden_states,
+            encoder_attention_mask=encoder_attention_mask,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
+            methylation_ids=methylation_ids,
+            age_ids=age_ids,
+        )
+        sequence_output = outputs[0]
+
+        logits = self.classifier(sequence_output)
+
+        loss = None
+        if labels is not None:
+            loss = self.loss_fct(logits.view(-1, self.num_labels), labels.view(-1, self.num_labels))
+
+        return SequenceClassifierOutput(
+            loss=loss,
+            logits=logits,
+            hidden_states=outputs.hidden_states,
+            attentions=outputs.attentions,
+        )
